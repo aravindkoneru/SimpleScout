c9042a50ea5a8b12c5bdbc7dd9016305c124126c
Aravind Koneru <aravind.b.koneru@gmail.com>
2015-05-01 23:37:01 -0400

    Added functionality to display events a team has been in

--- ScoutingComplex2015/apiReqest.js
+++ ScoutingComplex2015/apiReqest.js
@@ -1,29 +1,53 @@
 var http = require('http');
 var file = require('read-file');
+var u = require('underscore');
 
-//'Basic ' + new Buffer(username + ':' + password).toString('base64');
 
+//do not change this
 var auth = file.readFileSync('authkey.txt');
 
+var teamData = {};
 
-var options = {
-  host: 'private-945e3-frcevents.apiary-proxy.com',
-  path: '/api/v1.0/2015',
-  headers:
-    {Authorization: 'Basic ' + auth}
-};
-
-callback = function(response) {
-  var str = '';
-
-
-  response.on('data', function(chunk) {
-    str += chunk;
-  });
-
-  response.on('end', function() {
-    console.log(str);
-  });
+function scrapeTeamStats(teamNumber){
+  getEvents(teamNumber);
+  //u.delay(console.log(bigEventData), 1000);
 }
 
-http.request(options, callback).end();
+
+function storeEventData(teamNumber, parsedEventData){
+  teamData['events'] = {};
+  for(var x = 0; x < parsedEventData["Events"].length; x++){
+    var current = parsedEventData["Events"][x];
+    teamData['events'][current.code] = current.name;
+  }
+
+  console.log(teamData);
+
+}
+
+function getEvents(teamNumber){
+  var options = {
+    host: 'private-945e3-frcevents.apiary-proxy.com',
+    path: '/api/v1.0/events/2015?teamNumber=' + teamNumber,
+    headers:
+      {Authorization: 'Basic ' + auth}
+  };
+
+  callback = function(response) {
+    var str = '';
+
+
+    response.on('data', function(chunk) {
+      str += chunk;
+    });
+
+    response.on('end', function() {
+      var parsedEventData = JSON.parse(str);
+      storeEventData(teamNumber, parsedEventData);
+    });
+  }
+
+  http.request(options, callback).end();
+}
+
+getEvents(1923);
