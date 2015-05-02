42e98d4b74ee82c2502d77ea01ce421d050ede6f
Aravind Koneru <aravind.b.koneru@gmail.com>
2015-05-02 11:56:45 -0400

    can now access all the raw stats for a team's entire season

--- ScoutingComplex2015/apiReqest.js
+++ ScoutingComplex2015/apiReqest.js
@@ -10,19 +10,6 @@ var teamData = {};
 
 function scrapeTeamStats(teamNumber){
   getEvents(teamNumber);
-  //u.delay(console.log(bigEventData), 1000);
-}
-
-
-function storeEventData(teamNumber, parsedEventData){
-  teamData['events'] = {};
-  for(var x = 0; x < parsedEventData["Events"].length; x++){
-    var current = parsedEventData["Events"][x];
-    teamData['events'][current.code] = current.name;
-  }
-
-  console.log(teamData);
-
 }
 
 function getEvents(teamNumber){
@@ -50,4 +37,63 @@ function getEvents(teamNumber){
   http.request(options, callback).end();
 }
 
-getEvents(1923);
+
+function storeEventData(teamNumber, parsedEventData){
+  teamData['events'] = {};
+  for(var x = 0; x < parsedEventData["Events"].length; x++){
+    var current = parsedEventData["Events"][x];
+    teamData['events'][current.code] = {};
+    teamData['events'][current.code]['event_name'] = current.name
+  }
+
+  //console.log(teamData);
+  for(var eventCode in teamData['events']){
+    if(teamData['events'].hasOwnProperty(eventCode)){
+      getRawScores(teamNumber, eventCode);
+    }
+  }
+}
+
+function getRawScores(teamNumber, eventCode){
+  var options = {
+    host: 'private-945e3-frcevents.apiary-proxy.com',
+    path: '/api/v1.0/rankings/2015/' + eventCode + '?teamNumber=' + teamNumber,
+    headers:
+      {Authorization: 'Basic ' + auth}
+  };
+
+  callback = function(response){
+    var str = '';
+
+    response.on('data', function(chunk){
+       str += chunk;
+    });
+
+    response.on('end', function(){
+      var parsedScoreData = JSON.parse(str);
+      storeScoreData(teamNumber, eventCode, parsedScoreData);
+    });
+  }
+
+  http.request(options, callback).end();
+}
+
+function storeScoreData(teamNumber, eventCode, parsedScoreData){
+  var rawStats = parsedScoreData['Rankings'][0];
+  //console.log(rawStats);
+
+  teamData['events'][eventCode]['raw_stats'] = {};
+  //console.log(teamData['events'][eventCode]['raw_stats'])
+
+  for(var key in rawStats){
+    if(rawStats.hasOwnProperty(key)){
+      teamData['events'][eventCode]['raw_stats'][key] = rawStats[key];
+    }
+  }
+
+  //console.log(JSON.stringify(teamData));
+}
+
+scrapeTeamStats(1923);
+var log = u.bind(console.log, console);
+u.delay(log, 3000, teamData);
