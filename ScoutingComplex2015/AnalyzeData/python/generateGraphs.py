d712d61cb9e1e96484f51b9a993d4c693b85c0e6
Aravind Koneru <aravind.b.koneru@gmail.com>
2015-06-01 21:56:44 -0400

    Made the graph generation code a function. Also linted the python code and it is now up to pep-8 standards

--- ScoutingComplex2015/AnalyzeData/python/generateGraphs.py
+++ ScoutingComplex2015/AnalyzeData/python/generateGraphs.py
@@ -1,11 +1,8 @@
 import pygal
 import json
 
-from pygal.style import DarkSolarizedStyle
+from pygal.style import NeonStyle
 from pprint import pprint
-
-with open('../../collectedJSON/team_11/dataPoints.json') as data_file:
-    data = json.load(data_file)
 
 
 def allZero(someArray):
@@ -15,49 +12,64 @@ def allZero(someArray):
     return True
 
 
-line_chart = pygal.Line(style=DarkSolarizedStyle)
 
-line_chart.title = 'Team 11 Quantatative Stats'  #@TODO: Should auto to team
-line_chart.x_labels = map(str, range(1, 13))
-line_chart.x_title = "Match Number"
-line_chart.y_title = "Interactions with Respective Object"
+def makeGraphs(teamNumber):
+    with open('../../collectedJSON/team_' + str(teamNumber) + '/dataPoints.json') as data_file:
+        data = json.load(data_file)
 
-line_chart.show_y_labels = True
-line_chart.show_dots = True
+    line_chart = pygal.Line(style=NeonStyle)
 
-max = -100000000
-for item in data:
-    if(type(data[item][0]) is int and allZero(data[item]) == False):
-        for num in data[item]:
-            if(num > max):
-                max = num
-        line_chart.add(item, data[item])
+    line_chart.title = 'Team ' + str(teamNumber) + ' Quantatative Stats'
+    line_chart.x_labels = map(str, range(1, 13))
+    line_chart.x_title = "Match Number"
+    line_chart.y_title = "Interactions with Respective Object"
 
-line_chart.y_labels = map(str, range(1, max+1))
-line_chart.render_to_file('lineChart.svg')
+    line_chart.show_y_labels = True
+    line_chart.show_dots = True
 
+    max = -100000000
+    for item in data:
+        if(type(data[item][0]) is int and allZero(data[item]) == False):
+            for num in data[item]:
+                if(num > max):
+                    max = num
+            line_chart.add(item, data[item])
 
-bar_chart = pygal.Bar(style=DarkSolarizedStyle)
-bar_chart.title = 'Qualitative Data for team 11' #@TODO: Should auto to team
-barCompleted = []
-barFailed = []
-barNames = []
+    line_chart.y_labels = map(str, range(1, max+1))
 
-for item in data:
-    if(type(data[item][0]) is not int):
-        barNames.append(item)
-        completed = 0
-        failed = 0
-        for boolVal in data[item]:
-            if boolVal is True:
-                completed += 1
-            else:
-                failed += 1
-        barCompleted.append(completed)
-        barFailed.append(failed)
+    fileName = 'team' + str(teamNumber) + 'Quant.svg'
+    line_chart.render_to_file(fileName)
 
-bar_chart.add('Successful', barCompleted)
-bar_chart.add('Failed', barFailed)
-bar_chart.x_labels = barNames
+    """This is the start of the qualitative graph data"""
 
-bar_chart.render_to_file('barChart.svg')
+    bar_chart = pygal.Bar(style=NeonStyle)
+
+    bar_chart.title = 'Team ' + str(teamNumber) + ' Qualitative Stats'
+    bar_chart.y_title = "Times Action wwas Performed"
+    bar_chart.x_title = "Event Name"
+
+    barCompleted = []
+    barFailed = []
+    barNames = []
+
+    for item in data:
+        if(type(data[item][0]) is not int):
+            barNames.append(item)
+            completed = 0
+            failed = 0
+            for boolVal in data[item]:
+                if boolVal is True:
+                    completed += 1
+                else:
+                    failed += 1
+            barCompleted.append(completed)
+            barFailed.append(failed)
+
+    bar_chart.add('Successful', barCompleted)
+    bar_chart.add('Failed', barFailed)
+    bar_chart.x_labels = barNames
+
+    fileName = 'team' + str(teamNumber) + 'Qual.svg'
+    bar_chart.render_to_file(fileName)
+
+makeGraphs(11);
